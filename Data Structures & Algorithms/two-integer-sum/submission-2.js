3af46059abeeedf2a12728f62ae39a285b267d57
parth jain <parthjain98@yahoo.co.in>
2026-04-22 15:52:39 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = {};
+        for (let i = 0; i < nums.length; i++) {
+            if (map[target - nums[i]] !== undefined) {
+                return [i, map[target - nums[i]]];
+            }
+            map[nums[i]] = i;
+        }
+    }
+}
